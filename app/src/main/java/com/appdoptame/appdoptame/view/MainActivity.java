0d8a9051f86a0e7589dffb0e572d9d6cbd8b1120
jmmunoza <jmmunoza@eafit.edu.co>
2022-07-27 20:13:47 -0500

    Prueba

--- app/src/main/java/com/appdoptame/appdoptame/view/MainActivity.java
+++ app/src/main/java/com/appdoptame/appdoptame/view/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //addsfsf
+        //addsfsfd
 
 
      //   PRUEBA FIRESTORE (LA BASE DE DATOS)
