0b3e9842e7a02b125a16240358bc38d63ccca5e4
jmmunoza <jmmunoza@eafit.edu.co>
2022-08-15 13:02:13 -0500

    Avances login Facebook

--- app/src/main/java/com/appdoptame/appdoptame/data/repository/UserRepository.java
+++ app/src/main/java/com/appdoptame/appdoptame/data/repository/UserRepository.java
@@ -1,11 +1,18 @@
 package com.appdoptame.appdoptame.data.repository;
 
+import android.content.Intent;
+
+import androidx.fragment.app.Fragment;
+
 import com.appdoptame.appdoptame.data.listener.CompleteListener;
 import com.appdoptame.appdoptame.model.User;
+import com.facebook.CallbackManager;
 
 public interface UserRepository {
     void login(String email, String password, CompleteListener listener);
-    void loginGoogle(CompleteListener listener);
+    void loginGoogle(Fragment fragment);
+    void loginGoogleResult(Intent data, CompleteListener listener);
+    void loginFacebook(Fragment fragment, CallbackManager callbackManager, CompleteListener listener);
     void singOut(CompleteListener listener);
     void singUp(String email, String password, String confirmPassword, User user, CompleteListener listener);
 }
