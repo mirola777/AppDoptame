72cce94c443eb7f122688667546a075f9c201dfb
Guerrita <53787821+Guerrita@users.noreply.github.com>
2022-09-01 13:52:45 -0500

    Se crea el LoadComponents para el FragmentPostPet y se crea el campo de descripcion que no habia sido agredado

--- app/src/main/java/com/appdoptame/appdoptame/view/fragment/FragmentPostPet.java
+++ app/src/main/java/com/appdoptame/appdoptame/view/fragment/FragmentPostPet.java
@@ -5,16 +5,47 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.appdoptame.appdoptame.R;
+import com.appdoptame.appdoptame.data.firestore.UserRepositoryFS;
+import com.appdoptame.appdoptame.data.listener.CompleteListener;
+import com.appdoptame.appdoptame.model.Person;
+import com.appdoptame.appdoptame.model.Pet;
+import com.appdoptame.appdoptame.util.EditTextExtractor;
+import com.appdoptame.appdoptame.util.StatusBarHeightGetter;
+import com.appdoptame.appdoptame.view.fragmentcontroller.FragmentController;
+import com.appdoptame.appdoptame.view.fragmentcontroller.SetFragmentMain;
 
 import java.util.concurrent.TimeUnit;
 
 public class FragmentPostPet extends Fragment {
+
+    private ConstraintLayout statusBar;
+    private ImageButton      backButton;
+    private EditText         nameField;
+    private RadioButton      typeCatField;
+    private RadioButton      typeDogField;
+    private RadioButton      typeSterilizedField;
+    private RadioButton      typeNotSterilizedField;
+    private String           typeField;
+    private boolean          sterilizedField;
+    private EditText         breedField;
+    private EditText         ageField;
+    private EditText         sexField;
+    private EditText         sizeField;
+    private EditText         weightField;
+    private EditText         descriptionField;
+    private TextView         registerButton;
+
     @SuppressLint("InflateParams")
     @Nullable
     @Override
@@ -28,5 +59,88 @@ public class FragmentPostPet extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void loadComponents(){
+        statusBar            = requireView().findViewById(R.id.create_pet_status_bar);
+        backButton           = requireView().findViewById(R.id.create_pet_back_button);
+        nameField            = requireView().findViewById(R.id.create_pet_name_field);
+        typeCatField         = requireView().findViewById(R.id.select_pet_cat_field);
+        typeDogField         = requireView().findViewById(R.id.select_pet_dog_field);
+        breedField           = requireView().findViewById(R.id.create_pet_breed_field);
+        ageField            = requireView().findViewById(R.id.create_pet_age_field);
+        sexField             = requireView().findViewById(R.id.create_pet_sex_field);
+        sizeField            = requireView().findViewById(R.id.create_pet_size_field);
+        typeSterilizedField  = requireView().findViewById(R.id.select_pet_sterilized_yes_field);
+        typeNotSterilizedField  = requireView().findViewById(R.id.select_pet_sterilized_no_field);
+        weightField          = requireView().findViewById(R.id.create_pet_weight_field);
+        descriptionField          = requireView().findViewById(R.id.create_pet_description_field);
+        registerButton       = requireView().findViewById(R.id.create_pet_create_pet_button);
+
+        loadStatusBar();
+        loadBackButton();
+        loadRegisterButton();
+    }
+
+    private void loadRegisterButton(){
+        registerButton.setOnClickListener(v -> {
+            try {
+                /*String name            = EditTextExtractor.get(nameField);
+                String lastName        = EditTextExtractor.get(lastNameField);
+                String phone           = EditTextExtractor.get(phoneField);
+                long   age             = Long.parseLong(EditTextExtractor.get(ageField));
+                String identification  = EditTextExtractor.get(CCField);
+*/
+                if(typeCatField.isSelected()){
+                    typeField = "Gato";
+                }
+                if(typeDogField.isSelected()){
+                    typeField = "Perro";
+                }
+                breedField           = requireView().findViewById(R.id.create_pet_breed_field);
+                sexField              = requireView().findViewById(R.id.create_pet_sex_field);
+                sizeField              = requireView().findViewById(R.id.create_pet_size_field);
+                if(typeSterilizedField.isSelected()){
+                    sterilizedField = true;
+                }
+                else if(typeNotSterilizedField.isSelected()){
+                    sterilizedField = false;
+                }
+
+                String city            = "Medellin";  // Temporalmente mientras añadimos mas ciudades
+                String department      = "Antioquia"; // Temporalmente mientras añadimos mas departamentos
+
+                //Person newPet = new Pet();
+
+                // Se crea el usuario y se envía a la base de datos
+
+                /*UserRepositoryFS.getInstance().createPerson(newUser, new CompleteListener() {
+                    @Override
+                    public void onSuccess() {
+                        FragmentController.removeAllFragments();
+                        SetFragmentMain.set();
+                    }
+
+                    @Override
+                    public void onFailure() {
+
+                    }
+                });*/
+            } catch (Exception e){
+
+            }
+        });
+    }
+
+    private void loadBackButton(){
+        backButton.setOnClickListener(v -> FragmentController.onBackPressed());
+    }
+
+    private void loadStatusBar(){
+        // Añadiendo padding superior en base a la altura de la barra de notificaciones
+        ConstraintLayout.LayoutParams params =
+                (ConstraintLayout.LayoutParams) statusBar.getLayoutParams();
+        params.topMargin = StatusBarHeightGetter.get();
+        statusBar.setLayoutParams(params);
     }
 }
